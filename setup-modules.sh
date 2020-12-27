e958e744bf5c4f8bd4e4e14e80b66205222c4818
Ashkan Mirzaee <44558927+ashki23@users.noreply.github.com>
2020-12-27 13:10:16 -0600

    Create setup-modules.sh

---
+++ setup-modules.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+## --------------------------
+## By Ashkan Mirzaee Nov 2020
+## --------------------------
+
+core=( "/opt/spack/share/spack/lmod/linux-centos7-x86_64/Core"
+       "/opt/spack/share/spack/lmod/linux-centos7-x86_64/gcc/9.3.0" 
+       "/opt/spack/share/spack/lmod/linux-centos7-x86_64/openmpi/4.0.5-twjcwul/gcc/9.3.0" )
+
+##rm -r /opt/modulefiles/*
+
+for dir in ${core[*]}; do
+    for i in $(ls $dir); do
+	if echo $i | grep -Pqv "^lib.*|^util-.*|^perl-.*|^py-.*|^xcb-.*|^go-.*|^at-.*|^docbook-.*|.*proto$|.*font.*"; then
+	    ver=$(echo $dir/$i/* | tr " " "\n" | grep -Po "(?<=/$i/).*");
+	    if echo $ver | grep -q "9.3.0/gcc/"; then 
+		ver=$(echo $ver | grep -Po "(?<=9.3.0/gcc/).*"); 
+	    fi
+	    install -dvp /opt/modulefiles/$i;
+	    for j in $ver; do
+		k=$(echo $j | grep -Po ".*(?=-\w{4}\.lua)");
+		ln -sf $dir/$i/$j* /opt/modulefiles/$i/$k.lua;
+	    done
+	fi
+    done
+done 
