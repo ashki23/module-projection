8242d5259137812d802d2ec9d1cb78ebd3750721
Ashkan Mirzaee <44558927+ashki23@users.noreply.github.com>
2020-12-27 14:27:03 -0600

    Update setup-modules.sh

--- setup-modules.sh
+++ setup-modules.sh
@@ -9,7 +9,7 @@ modulefiles="/opt/modulefiles"
 
 for dir in ${core[*]}; do
     for i in $(ls $dir); do
-	if echo $i | grep -Pqv "^lib.*|^util-.*|^perl-.*|^py-.*|^xcb-.*|^go-.*|^at-.*|^docbook-.*|.*proto$|.*font.*|^autoconf.*|^automake.*|harfbuzz|cairo"; then
+	if echo $i | grep -Pqv "^lib.*|^util-.*|^perl-.*|^py-.*|^xcb-.*|^go-.*|^at-.*|^docbook-.*|.*proto$|.*font.*"; then
 	    ver=$(echo $dir/$i/* | tr " " "\n" | grep -Po "(?<=/$i/).*");
 	    if echo $ver | grep -q "9.3.0/gcc/"; then 
 		ver=$(echo $ver | grep -Po "(?<=9.3.0/gcc/).*"); 
@@ -21,4 +21,4 @@ for dir in ${core[*]}; do
 	    done
 	fi
     done
-done 
+done
